32b0e965e1b30626d87eb62cd884c2ceee1e5fad
Ram1285 <yepuri.ramcharan12@gmail.com>
2025-10-25 22:33:57 +0530

    Small excetion.py change

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,20 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
+def error_message_detail(error,error_detail):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+    error_message = (
+        f"Error occurred in python script name [{file_name}] "
+        f"line number [{exc_tb.tb_lineno}] error message [{error}]"
+    )
 
     return error_message
 
     
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_detail):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
